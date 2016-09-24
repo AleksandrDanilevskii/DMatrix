f1f233bb498d5654847c304ba59fb8cdcffe0699
AleksandrDanilevskii <danilevskii.aleksandr97@gmail.com>
2016-09-25 01:51:50 +0300

    Добавлено чтение из файла и произвольные размеры

--- src/DMatrix/DMatrix.java
+++ src/DMatrix/DMatrix.java
@@ -1,64 +1,103 @@
 package DMatrix;
 
 import java.util.Scanner;
-import java.io.*;
+//import java.io.*;
+import java.io.IOException;
+//import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class DMatrix {
     public static void main(String[] args) {
-        int a1;
-        int a2;
-        int b1;
-        int b2;
-        int[][] mat1, mat2, mat3;
-        mat1 = new int[100][100];
-        mat2 = new int[100][100];
-        mat3 = new int[100][100];
-
+        int[][] a = new int[100][100];
+        int[][] b = new int[100][100];
+        int[][] c = new int[100][100];
+        int widthA = 0;
+        int widthB = 0;
+        int heightA = 0;
+        int heightB = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите размер матрицы A: ");
-        a1 = sc.nextInt();
-        b1 = sc.nextInt();
-        System.out.println("Введите размер матрицы B: ");
-        a2 = sc.nextInt();
-        b2 = sc.nextInt();
-        if (b1 != a2) {
-            System.out.println("Такие матрицы нельзя умножать.");
-            System.exit(0);
-        } else {
 
-            System.out.println("Введите элементы матрицы A: ");
-            for (int i = 0; i <= 2; i++) {
-                for (int j = 0; j <= 2; j++) {
-                    System.out.print("а[" + i + "][" + j + "]=");
-                    mat1[i][j] = sc.nextInt(); // считывает целое число с потока ввода и сохраняем в переменную
+        //чтение матрицы А:
+        try {
+            File file = new File("C:\\Users\\Notebook\\IdeaProjects\\DMatrix\\DMatrixА.txt");
+            FileReader fr = new FileReader(file);//создаем объект
+            BufferedReader reader = new BufferedReader(fr);//создаем BufferedReader с существующего FileReader для построчного считывания
+            String line = reader.readLine();//считываем первую строчку
+            String[] str = line.split(" ");
+            widthA = str.length; //число столбцов
+            heightA = 0; //номер строки
+            while (line != null) {
+                str = line.split(" "); //создаем массив s слов
+                for (int j = 0; j < widthA; j++) { //заполнение строки массива
+                    int number = Integer.parseInt(str[j]); //strtoint
+                    a[heightA][j] = number;
                 }
+                line = reader.readLine();// считываем остальные строки в цикле
+                heightA++;
             }
-
-            System.out.println("Введите элементы матрицы B: ");
-            for (int i = 0; i <= 2; i++) {
-                for (int j = 0; j <= 2; j++) {
-                    System.out.print("b[" + i + "][" + j + "]=");
-                    mat2[i][j] = sc.nextInt(); // считывает целое число с потока ввода и сохраняем в переменную
-                }
-            }
-            int i, j, t;
-            for (i = 0; i <= 2; i++) {
-                for (j = 0; j <= 2; j++) {
-                    for (t = 0; t <= 2; t++) {
-                        mat3[i][j] += mat1[i][t] * mat2[t][j];
-                    }
-                }
-            }
-
-            System.out.println();
-            System.out.println("A*B=");
-            for (i = 0; i <= 2; i++) {
-                for (j = 0; j <= 2; j++) {
-                    System.out.print(mat3[i][j] + "\t");
+            for (int i = 0; i < heightA; i++) {//печать массива
+                for (int j = 0; j < widthA; j++) {
+                    System.out.print(a[i][j] + "\t");
                 }
                 System.out.println();
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
+        System.out.println();
+        try { //чтение матрицы B:
+            File file = new File("C:\\Users\\Notebook\\IdeaProjects\\DMatrix\\DMatrixB.txt");
+            FileReader fr = new FileReader(file);//создаем объект FileReader для объекта File
+            BufferedReader reader = new BufferedReader(fr);//создаем BufferedReader с существующего FileReader для построчного считывания
+            String line = reader.readLine();// считаем сначала первую строку
+            String[] str = line.split(" ");
+            widthB = str.length; //число столбцов
+            heightB = 0; //число строк
+            while (line != null) {
+                str = line.split(" "); //создаем массив s слов
+                for (int j = 0; j < widthB; j++) { //заполнение строки массива
+                    int number = Integer.parseInt(str[j]); //strtoint
+                    b[heightB][j] = number;
+                }
+                line = reader.readLine();// считываем остальные строки в цикле
+                heightB++;
+            }
+            for (int i = 0; i < heightB; i++) { //печать массива
+                for (int j = 0; j < widthB; j++) {
+                    System.out.print(b[i][j] + "\t");
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (heightB != widthA) { //умножение
+            System.out.println("Умножение таких матриц невозможно.");
+            System.exit(0);
+        } else {
+            for (int i = 0; i < heightA; i++) {
+                for (int j = 0; j < widthB; j++) {
+                    //c[i][j]=0;
+                    for (int t = 0; t < widthA; t++) {
+                        c[i][j] += a[i][t] * b[t][j];
+                    }
+                }
+            }
+        }
+        System.out.println(); //вывод умножение
+        System.out.println("A*B=");
+        for (int i = 0; i < heightA; i++) {
+            for (int j = 0; j < widthB; j++) {
+                System.out.print(c[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
